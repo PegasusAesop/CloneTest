646f3f32c8a4472296dd559bee8156af6d93dcf9
PegasusAesop <hothome99@sina.com.cn>
2018-12-14 21:30:22 +0800

    add static method();

--- src/main/java/com/pegasus/clonetest/test/CloneTest.java
+++ src/main/java/com/pegasus/clonetest/test/CloneTest.java
@@ -2,6 +2,10 @@ package com.pegasus.clonetest.test;
 
 public class CloneTest {
 
+	public static void methd()
+	{
+		System.out.println("I am is static method().");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my clonetest");
