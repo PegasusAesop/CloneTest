1e827167576b624d393a172f6d6f436e27b4888a
PegasusAesop <hothome99@sina.com.cn>
2018-12-14 11:34:34 +0800

    add new files

---
+++ src/main/java/com/pegasus/clonetest/test/CloneTest.java
@@ -0,0 +1,10 @@
+package com.pegasus.clonetest.test;
+
+public class CloneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my clonetest");
+	}
+
+}
